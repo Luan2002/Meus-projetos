3a38cbecb9764c5e91349f1e95a171d260c1c292
Luan Gomes Da Silva <77366431+Luan2002@users.noreply.github.com>
2021-01-18 08:27:47 -0300

    Calculadora Python
    Calculadora Python de adição, subtração, multiplicação,  divisão , divisão inteira, resto da divisão e potência.

---
+++ EX 001.py
@@ -0,0 +1,17 @@
+n1 = int (input("Digite um valor:"))
+n2 = int (input("Digite outro valor:"))
+soma = n1 + n2
+div = n1 / n2
+div1 = n1 // n2
+sub = n1 - n2
+pot = n1 ** n2
+multi = n1 * n2
+rest = n1 % n1
+print(" O Resultado da Soma de {} e {} é : {} ".format( n1 , n2 , soma))
+print(" O Resultado da Divisão de {} e {} é : {}".format(n1 , n2, div))
+print(" O Resultado da Divisão inteira de {} e {} é: {}".format(n1, n2, div1))
+print(" O Resultado da Subtração de {} e {} é: {}".format(n1 , n2 , sub))
+print(" O Resultado da Potência de {} e {} é: {}".format(n1 , n2 , pot))
+print(" O Resultado da Multiplicação de {} e {} é: {} ".format(n1, n2, multi))
+print(" O Resultado do Resto da divisão de {} e {} é:{}".format(n1, n2, rest))
+
