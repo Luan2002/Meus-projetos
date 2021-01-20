a7a3b5d4f5093c733df0baa19df4a683592c0bbc
Luan Gomes Da Silva <77366431+Luan2002@users.noreply.github.com>
2021-01-20 10:53:41 -0300

    Add files via upload

---
+++ Calculadora Sen, Cos e Tan.py
@@ -0,0 +1,9 @@
+import  math
+n = float(input("Digite quantos graus você deseja:"))
+sen = math.sin(math.radians(n))
+cos = math.cos(math.radians(n))
+tan = math.tan(math.radians(n))
+print("O seno de {} é: {:.1f}".format(n , sen ))
+print("O coseno de {} é: {:.1f}".format(n, cos))
+print('O coseno de {} é: {:.1f}'.format(n , tan))
+
